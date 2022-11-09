f5458952c0dea4a3eaee4604daa0d26f40a8efae
chanminmaxscaler <jcm04@naver.com>
2022-11-09 22:36:39 +0900

    feat: EnumExercise, Rainbow 클래스를 추가해 Enum을 연습한다

---
+++ code/src/main/java/exercise/enumexercise/EnumExercise.java
@@ -0,0 +1,7 @@
+package exercise.enumexercise;
+
+public class EnumExercise {
+    public static void main(String[] args) {
+        System.out.println(Rainbow.YELLOW.getValue());
+    }
+}
---
+++ code/src/main/java/exercise/enumexercise/Rainbow.java
@@ -0,0 +1,19 @@
+package exercise.enumexercise;
+
+enum Rainbow {
+    RED(3),
+    ORANGE(10),
+    YELLOW(21),
+    GREEN(5),
+    BLUE(1),
+    INDIGO(-1),
+    VIOLET(-11);
+
+    private final int value;
+    Rainbow(int value) {
+        this.value = value;
+    }
+    public int getValue() {
+        return value;
+    }
+}
